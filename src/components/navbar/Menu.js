f26cc0a3ea125fdeba377dbfccc2234f6a7b74fe
leomacode <forza.leoma@gmail.com>
2022-02-18 16:58:36 +0100

    fix bugs in Navbar

--- src/components/navbar/Menu.js
+++ src/components/navbar/Menu.js
@@ -6,8 +6,8 @@ function Menu({ menu_className }) {
     <div className={menu_className}>
       <Navlink section__name="home" section__location="home" />
       <Navlink section__name="Waht is GPT3" section__location="wgpt3" />
-      <Navlink section__name="Open AI" section__location="possibility" />
       <Navlink section__name="Case Studies" section__location="features" />
+      <Navlink section__name="Open AI" section__location="possibility" />
       <Navlink section__name="Library" section__location="blog" />
     </div>
   );
